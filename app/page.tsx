c89d28a210cd1f47d372de2c6b3b91a951174a59
Eduardo Bachosky <voronll@protonmail.com>
2026-02-16 10:18:19 -0300

    feat: inserção de logo e alteração do texto da landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="rounded-lg border border-zinc-200 bg-white p-8 shadow-lg dark:border-zinc-800 dark:bg-zinc-900">
-        <p className="text-center text-lg font-medium text-zinc-800 dark:text-zinc-200">
-          Site em desenvolvimento - Em breve mais informações.
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FAFAFA] font-sans">
+      <div className="mb-8">
+        <Image
+          src="/images/logo.png"
+          alt="AllRecon"
+          width={500}
+          height={500}
+          priority
+        />
+      </div>
+      <div className="rounded-lg border-2 border-[#233764] bg-[#FAFAFA] p-8 shadow-lg">
+        <p className="text-center text-lg font-medium text-[#233764]">
+          Landpage em desenvolvimento - Em breve mais informações.
         </p>
       </div>
     </div>
